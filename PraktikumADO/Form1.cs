dc1c66d2626ae930e3a6ddd4020747787e82da48
Ayuningtyas24 <adyahseptiani@gmail.com>
2026-03-31 15:49:57 +0700

    Menambahkan penanganan error (catch) pada btnHitungMhs

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -69,6 +69,11 @@ namespace PraktikumADO
 
                 conn.Close();
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
         }
     }
      
